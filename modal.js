d5bc955dcd282b75cf84beb0a97b0545f9c32152
Sagarsolanki97 <118007645+Sagarsolanki97@users.noreply.github.com>
2023-04-17 12:33:26 +0530

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,18 @@
+function openModal(){
+    var container = document.getElementById("modal-container-id");
+    container.style.display = "flex";
+}
+
+function closeModal(){
+    var container = document.getElementById("modal-container-id");
+    container.style.display = "none";
+
+}
+
+// function closemodal(){
+//     var container = document.getElementById("modal-container-id");
+//     if(container.style.display== "flex"){
+//         container.style.display = "none";
+//     }
+// }
+
